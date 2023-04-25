5d05ad7edfcbf0b203be2c195cdcb40fab2c58f3
Christian <macheadway@gmail.com>
2023-04-25 09:09:34 -0700

    Ffth commit

---
+++ Chris/printf_functions.c
@@ -0,0 +1,145 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+/**
+ * print_strings - printf() with string parameter
+ * @a: string pointer to be printed
+ * @base: base
+(* Return: Return characters printed
+ */
+int print_strings(char *a)
+{
+	int counter = 0;
+
+	while (a[counter] != '\0')
+	{
+		putchar(a[counter]);
+		counter++;
+	}
+	return (counter);
+}
+
+/**
+ * my_reverse - Reverse string
+ * @str: String to reverse
+ * @len: strlen(str)
+ * Return: Reversed string
+ */
+void my_reverse(char str[], int len)
+{
+	int start, end;
+	char temp;
+
+	for (start = 0, end = len - 1; start < end; start++, end--)
+	{
+		temp = *(str + start);
+		*(str + start) = *(str + end);
+		*(str + end) = temp;
+	}
+}
+
+
+/**
+ * my_itoa - itoa(int num, char* str, int base). itoa with int
+ * @ap: Number to be converted to string
+ * @b: strlen(str)
+(* Return: Print string
+ */
+int my_itoa(va_list ap, char b)
+{
+	int base, i = 0;
+
+	int num = va_arg(ap, int);
+	char charArray[128];
+	char *str = charArray;
+	bool isNegative = false;
+
+	if (b == 'b')
+		base = 2;
+	else if (b == 'u' || b == 'i' || b == 'd')
+		base = 10;
+	else if (b == 'o')
+		base = 8;
+	else if (b == 'x')
+		base = 16;
+	if (num == 0)
+	{
+		str[i] = '0';
+		str[i + 1] = '\0';
+		return (0);
+	}
+	if (num < 0 && base == 10)
+	{
+		isNegative = true;
+		num = -num;
+	}
+	while (num != 0)
+	{
+		int rem = num % base;
+
+		str[i++] = (rem > 9) ? (rem - 10) + 'A' : rem + '0';
+		num = num / base;
+	}
+	if (isNegative)
+	{
+		str[i++] = '-';
+	}
+	str[i] = '\0';
+
+	my_reverse(str, i);
+	print_strings(str);
+	return (0);
+}
+
+ /**
+ * my_utoa - itoa(unsigned int num, char* str, int base).
+ * @num: unsigned int to be converted to string
+ * @str: Resulting string
+ * @base: strlen(str)
+(* Return: string
+ */
+int my_utoa(va_list ap, char b)
+{
+	int base, i = 0;
+	char charArray[128];
+	char *str = charArray;
+	unsigned int num = va_arg(ap, unsigned int);
+	bool isNegative = false;
+
+	if (b == 'b')
+		base = 2;
+	else if (b == 'u' || b == 'i' || b == 'd')
+		base = 10;
+	else if (b == 'o')
+		base = 8;
+	else if (b == 'x')
+		base = 16;
+	if (num == 0)
+	{
+		str[i] = '0';
+		str[i + 1] = '\0';
+		return (0);
+	}
+	while (num != 0)
+	{
+		int rem = num % base;
+
+		str[i++] = (rem > 9) ? (rem - 10) + 'A' : rem + '0';
+		num = num / base;
+	}
+
+	if (isNegative)
+	{
+		str[i++] = '-';
+	}
+
+	str[i] = '\0';
+	my_reverse(str, i);
+	print_strings(str);
+	return (0);
+}
+
